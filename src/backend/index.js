b5d5894a31d9e5994bd2dc77e7e0d5f5d3235eba
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-06 12:54:40 +0900

    API responds with database content

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Percolator = require('percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
 var server = Percolator({'port': port});
@@ -8,11 +9,19 @@ var server = Percolator({'port': port});
 server.route('/api/keywords',
 	{
 		GET: function (req, res) {
-			res.object({'foo': 'bar'}).send()
+			dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id', function (err, rows) {
+				if (err) {
+					console.log(err);
+					res.status.internalServerError(err);
+				} else {
+					console.log(rows);
+					res.collection(rows).send();
+				}
+			});
 		}
 	}
 );
 
-server.listen(function() {
+server.listen(function () {
 	console.log('Server started and listening on port', port);
 });
